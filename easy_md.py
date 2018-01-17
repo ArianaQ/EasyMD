18b42ca0f9c44bf0cac735fd1b0cc486955e869a
Ariana <Ari@Arianas-MacBook-Pro.local>
2018-01-17 14:28:29 -0400

    First PL Commit

---
+++ easy_md.py
@@ -0,0 +1,182 @@
+# reserved words
+# reserved = {
+#     'Header1': 'HEADER1',
+#     'Header2': 'HEADER2',
+#     'Header3': 'HEADER3',
+#     'Header4': 'HEADER4',
+#     'Header5': 'HEADER5',
+#     'Header6': 'HEADER6',
+#     'Italic': 'ITALIC',
+#     'Bold': 'BOLD',
+#     'Strike': 'STRIKE',
+#     'Bullet': 'BULLET',
+#     'Image': 'IMAGE',
+#     'Link': 'LINK',
+#     'User': 'USER',
+#     'Task': 'TASK'
+# }
+
+# Tokens
+tokens = [
+    'HEADER1',
+    'HEADER2',
+    'HEADER3',
+    'HEADER4',
+    'HEADER5',
+    'HEADER6',
+    'ITALIC',
+    'BOLD',
+    'STRIKE',
+    'BULLET',
+    'IMAGE',
+    'LINK',
+    'USER',
+    'TASK',
+    'RSQUARE_PAREN',
+    'LSQUARE_PAREN',
+    'TEXT',
+    'QUOTE',
+    'EMOJI',
+    'CODE',
+    'NUM',
+    'CLEAN_TEXT'
+    # 'SPACE'
+]  # + list(reserved.values())
+
+
+# Tokens functions
+# t_TEXT = r'[a-zA-Z0-9 ]+'
+# t_HEADER1 = r'Header1'
+# t_HEADER2 = r'Header2'
+# t_HEADER3 = r'Header3'
+# t_HEADER4 = r'Header4'
+# t_HEADER5 = r'Header5'
+# t_HEADER6 = r'Header6'
+# t_ITALICS = r'Italics'
+# t_BULLET = r'Bullet'
+# t_BOLD = r'Bold'
+# t_STRIKE = r'Strike'
+# t_IMAGE = r'Image'
+# t_LINK = r'Link'
+# t_USER = r'User'
+# t_TASK = r'Task'
+t_RSQUARE_PAREN = r'\]'
+t_LSQUARE_PAREN = r'\['
+t_ignore_SPACE = r'[ ]+'
+
+
+def t_HEADER1(t):
+    r'Header1'
+    return t
+
+
+def t_HEADER2(t):
+    r'Header2'
+    return t
+
+
+def t_HEADER3(t):
+    r'Header3'
+    return t
+
+
+def t_HEADER4(t):
+    r'Header4'
+    return t
+
+
+def t_HEADER5(t):
+    r'Header5'
+    return t
+
+
+def t_HEADER6(t):
+    r'Header6'
+    return t
+
+
+def t_BULLET(t):
+    r'Bullet'
+    return t
+
+
+def t_ITALIC(t):
+    r'Italic'
+    return t
+
+
+def t_BOLD(t):
+    r'Bold'
+    return t
+
+
+def t_STRIKE(t):
+    r'Strike'
+    return t
+
+
+def t_IMAGE(t):
+    r'Image'
+    return t
+
+
+def t_LINK(t):
+    r'Link'
+    return t
+
+
+def t_USER(t):
+    r'User'
+    return t
+
+
+def t_TASK(t):
+    r'Task'
+    return t
+
+
+def t_QUOTE(t):
+    r'Quote'
+    return t
+
+
+def t_EMOJI(t):
+    r'Emoji'
+    return t
+
+
+def t_CODE(t):
+    r'Code'
+    return t
+
+
+def t_NUM(t):
+    r'Num.'
+    return t
+
+
+def t_TEXT(t):
+    r'[ ]+[()a-zA-Z0-9 /!?_^:;=.><+-]+'
+    t.value = t.value.strip()
+    return t
+
+
+def t_CLEAN_TEXT(t):
+    r'[()a-zA-Z0-9 /!?_^:;=.><+-]+'
+    t.value = t.value.strip()
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+# Build the lexer
+import ply.lex as lex
+lex.lex()
---
+++ easy_md_parser.py
@@ -0,0 +1,411 @@
+import ply.yacc as yacc
+from easy_md import tokens
+import os.path
+
+md_code = ''
+bypass = ''
+previous_act = ''
+
+def p_expression(p):
+    '''expression : header 
+                | bold 
+                | italic
+                | strike
+                | user
+                | image
+                | bullet
+                | link
+                | quote
+                | emoji
+                | code
+                | num
+                | clean_text'''
+    new_line()
+    p[0] = p[1]
+
+
+def p_header(p):
+    '''header : LSQUARE_PAREN HEADER1  TEXT RSQUARE_PAREN
+            | LSQUARE_PAREN HEADER2  TEXT RSQUARE_PAREN
+            | LSQUARE_PAREN HEADER3  TEXT RSQUARE_PAREN
+            | LSQUARE_PAREN HEADER4  TEXT RSQUARE_PAREN
+            | LSQUARE_PAREN HEADER5  TEXT RSQUARE_PAREN
+            | LSQUARE_PAREN HEADER6  TEXT RSQUARE_PAREN
+    '''
+    if p[2] == 'Header1':
+        p[0] = header1_to_md(p[3])
+    elif p[2] == 'Header2':
+        p[0] = header2_to_md(p[3])
+    elif p[2] == 'Header3':
+        p[0] = header3_to_md(p[3])
+    elif p[2] == 'Header4':
+        p[0] = header4_to_md(p[3])
+    elif p[2] == 'Header5':
+        p[0] = header5_to_md(p[3])
+    else:
+        p[0] = header6_to_md(p[3])
+
+
+def p_clean_text(p):
+    '''clean_text : CLEAN_TEXT
+                    | CLEAN_TEXT compound 
+                    | CLEAN_TEXT compound TEXT
+                    | clean_text
+                    '''
+
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += p[1] + ' ' + bypass
+    start = -1
+    previous = -1
+    for i in range(len(md_code)):
+        if md_code[i:i + len(bypass)] == bypass:
+            previous = start
+            start = i
+
+    lmd_code = md_code[:previous]
+    rmd_code = md_code[previous + len(bypass):]
+    md_code = lmd_code + rmd_code
+
+    if len(p) == 4:
+        md_code += p[3]
+        p[0] = p[1] + bypass + p[3]
+    else:
+        p[0] = p[1] + bypass
+
+
+def p_compund(p):
+    '''compound : bold 
+                | italic
+                | strike
+                | user
+                | image
+                | link
+                | emoji
+                | bold clean_text
+                | italic clean_text
+                | strike clean_text
+                | user clean_text
+                | image clean_text
+                | link clean_text
+                | emoji clean_text'''
+
+
+def p_bold(p):
+    '''bold : LSQUARE_PAREN BOLD TEXT RSQUARE_PAREN'''
+    p[0] = bold_to_md(p[3])
+
+
+def p_italic(p):
+    '''italic : LSQUARE_PAREN ITALIC TEXT RSQUARE_PAREN'''
+    p[0] = italic_to_md(p[3])
+
+
+def p_strike(p):
+    '''strike : LSQUARE_PAREN STRIKE TEXT RSQUARE_PAREN'''
+    p[0] = strike_to_md(p[3])
+
+
+def p_user(p):
+    '''user : LSQUARE_PAREN USER TEXT RSQUARE_PAREN '''
+    p[0] = user_to_md(p[3])
+
+
+def p_image(p):
+    '''image : LSQUARE_PAREN IMAGE TEXT RSQUARE_PAREN'''
+    p[0] = image_to_md(p[3])
+
+
+def p_bullet(p):
+    '''bullet : LSQUARE_PAREN BULLET TEXT RSQUARE_PAREN'''
+    p[0] = bullet_to_md(p[3])
+
+
+def p_link(p):
+    '''link : LSQUARE_PAREN LINK TEXT  RSQUARE_PAREN'''
+    p[0] = link_to_md(p[3])
+
+
+def p_quote(p):
+    '''quote : LSQUARE_PAREN QUOTE TEXT RSQUARE_PAREN'''
+    p[0] = quote_to_md(p[3])
+
+
+def p_emoji(p):
+    '''emoji : LSQUARE_PAREN EMOJI TEXT RSQUARE_PAREN'''
+    p[0] = emoji_to_md(p[3])
+
+def p_code(p):
+    '''code : LSQUARE_PAREN CODE TEXT RSQUARE_PAREN'''
+    p[0] = code_to_md(p[3])
+
+def p_num(p):
+    '''num : LSQUARE_PAREN NUM TEXT RSQUARE_PAREN'''
+    p[0] = num_to_md(p[2], p[3])
+    p[0] = p[1]
+
+def new_line():
+    global md_code
+    md_code += '\n'
+
+def header1_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '# ' + text
+    previous_act = 'header'
+    return '# ' + text
+
+def header2_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '## ' + text
+    previous_act = 'header'
+    return '## ' + text
+
+def header3_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '### ' + text
+    previous_act = 'header'
+    return ' ### ' + text
+
+def header4_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '#### ' + text
+    previous_act = 'header'
+    return ' #### ' + text
+
+def header5_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '##### ' + text
+    previous_act = 'header'
+    return '##### ' + text
+
+def header6_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '###### ' + text + ' '
+    previous_act = 'header'
+    return ' ###### ' + text + ' '
+
+def bold_to_md(text):
+    global bypass
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '**' + text + '**' + ' '
+    bypass = '**' + text + '**' + ' '
+    previous_act = 'bold'
+    return ' ' + '**' + text + '**' + ' '
+
+def italic_to_md(text):
+    global bypass
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '*' + text + '*' + ' '
+    bypass = '*' + text + '*' + ' '
+    previous_act = 'italic'
+    return '*' + text + '*' + ' '
+
+def strike_to_md(text):
+    global bypass
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += ' ' + '~~' + text + '~~' + ' '
+    bypass = ' ' + '~~' + text + '~~' + ' '
+    previous_act = 'strike'
+    return '~~' + text + '~~'
+
+def user_to_md(text):
+    global bypass
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '@' + text + ' '
+    bypass = '@' + text + ' '
+    previous_act = 'user'
+    return '@' + text + ' '
+
+def image_to_md(text):
+    global bypass
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    bypass = '![alt text]' + text + ' '
+    md_code += '![alt text]' + text + ' '
+    previous_act = 'image'
+    return '![alt text]' + text + ' '
+
+def bullet_to_md(text):
+    global md_code
+    global previous_act
+    md_code += '* ' + text + ' '
+    previous_act = 'list'
+    return '* ' + text + ' '
+
+def link_to_md(text):
+    text = text.split('Text')
+    link = text[0]
+    text = text[1]
+    global bypass
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    bypass = '[' + text + ']' + '(' + link + ') '
+    md_code += '[' + text + ']' + '(' + link + ') '
+    previous_act = 'link'
+    return '[' + text + ']' + '(' + link + ') '
+
+def quote_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += '>' + text + ' '
+    previous_act = 'quote'
+    return '>' + text + ' '
+
+def emoji_to_md(text):
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += ':' + text + ':'
+    previous_act = 'emoji'
+    return '>' + text + ' '
+
+def code_to_md(text):
+    print(text)
+    temp = text.split('Language')
+    text = temp[0]
+    language = temp[1].strip()
+    print(language)
+    global md_code
+    global previous_act
+    if previous_act == 'list' or previous_act == 'quote':
+        md_code += '\n'
+    md_code += "'''" + language + ' ' + text + "'''"
+    previous_act = 'code'
+    return "'''" + language + ' ' + text + "'''"
+
+def num_to_md(num, text):
+    num = str(num.split('Num')[1])
+    global md_code
+    global previous_act
+    md_code += num + '. ' + text
+    previous_act = 'list'
+    return num + '.' + text
+
+def p_error(p):
+    print("Syntax error at '%s'" % p.value)
+
+def ask_for_path():
+    checkagain = True
+    while checkagain:
+        path = input("TYPE FILE TO CONVERT TO MD: ")
+
+        if (path.lower()=="exit"):
+            print('\nThanks for using EasyMD! Bye!');
+            exit()
+        elif ((os.path.exists(path))==False):
+            print("\nFILE DOESN'T EXIST, TRY AGAIN! | TO EXIT, TYPE EXIT\n")
+        elif (path.lower().endswith('.txt') == False):
+            print("\nFILE IS NOT .TXT, TRY AGAIN! | TO EXIT, TYPE EXIT\n")
+        else:
+            checkagain=False
+            return path
+
+def show_in_terminal(pa):
+    with open(path) as f:
+        for line in f:
+            result = parser.parse(line)
+
+    print('\n============================')
+    print('MD CODE TRANSLATION PREVIEW:')
+    print('============================')
+    print(md_code)
+
+def save_and_show(pa):
+    with open(path) as f:
+        for line in f:
+            result = parser.parse(line)
+
+    newfile = open("converted.md","w") 
+    newfile.write(md_code) 
+    newfile.close() 
+
+    print('\n============================')
+    print('MD CODE TRANSLATION PREVIEW:')
+    print('============================')
+    print(md_code)    
+
+def just_save(pa):
+    with open(path) as f:
+        for line in f:
+            result = parser.parse(line)
+
+    newfile = open("converted.md","w") 
+    newfile.write(md_code) 
+    newfile.close() 
+
+#BUILD PARSER
+parser = yacc.yacc()
+
+#USER INTERFACE
+print('\n==================')
+print('WELCOME TO EASYMD!')
+print('==================')
+print('AS A USER, YOU CAN CONVERT A FILE WITH EASYMD')
+print('SYNTAX TO MD CODE AND/OR TO AN MD FILE!')
+
+check = True;
+while (check):
+    print('\nWHAT DO YOU WANT TO DO?')
+    print('1 - Show translation in terminal.')
+    print('2 - Save translation to a new file.')
+    print('3 - Both! Show and save translation.')
+    print('4 - Exit.\n')
+
+    action = input()
+    if (action=="1"):
+        path = ask_for_path()
+        show_in_terminal(path)
+        check = False;
+    elif (action=="2"):
+        path = ask_for_path()
+        just_save(path)
+        check = False;
+    elif (action=="3"):
+        path = ask_for_path()
+        save_and_show(path)
+        check = False;
+    elif (action=="4"):
+        print('Thanks for using EasyMD! Bye!');
+        exit()
+    else:
+        print("INVALID! | TRY AGAIN!\n")
+
